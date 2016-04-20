1e7213c2a73a7df3d840ed14ed6a956ce509e856
jbain1 <jbainschool@gmail.com>
2016-04-20 14:31:13 +0000

    Finished get synonyms

--- get-synonyms.js
+++ get-synonyms.js
@@ -3,20 +3,32 @@ var request = require('request');
 var prompt = require('prompt');
 var colors = require('colors');
 
-var syn1 = new SynonymAPI("c272336453182631b44cf03c0671b805");
+var syn1 = new SynonymAPI("5ac6284f3455459ef6788ab04ddf82a6");
 
 prompt.start();
 prompt.message = colors.blue("What word shall we look up today?");
 prompt.get(['word'], function (err, result) {
     syn1.getSynonyms(result.word, function(word_obj){
-        ("noun" in word_obj) ? console.log(colors.magenta("Noun synonyms: " + word_obj.noun.syn.join(', '))) : null;
-        ("ant" in word_obj.noun) ? console.log("Noun antonyms: " + word_obj.noun.ant.join(', ')) : null;
-        ("verb" in word_obj) ? console.log(colors.cyan("Verb synonyms: " + word_obj.verb.syn.join(', '))
-        + "\n Verb antonyms: " + word_obj.verb.ant.join(', ')) : null;
-        ("adjective" in word_obj) ? console.log(colors.blue("Adjective synonyms: " + word_obj.adjective.syn.join(', '))
-         + "\n Adjective antonyms: " + word_obj.verb.ant.join(', ')) : null;
-        ("sim" in word_obj) ? console.log(colors.purple("Similar terms: " + word_obj.verb.syn.join(', '))) : null; 
+    if ("noun" in word_obj){
+       ("syn" in word_obj.noun) ? console.log(colors.magenta("Noun synonyms: " + word_obj.noun.syn.join(', '))) : null;
+       ("ant" in word_obj.noun) ? console.log("Noun antonyms: " + word_obj.noun.ant.join(', ')) : null;
+
+    };
+    
+    if ("verb" in word_obj){
+       ("syn" in word_obj.verb) ? console.log(colors.cyan("Verb synonyms: " + word_obj.verb.syn.join(', '))) : null;
+       ("ant" in word_obj.verb) ? console.log("Verb antonyms: " + word_obj.verb.ant.join(', ')) : null;
+    };
+    
+    if ("adjective" in word_obj){
+       ("syn" in word_obj.verb) ? console.log(colors.blue("Adjective synonyms: " + word_obj.adjective.syn.join(', '))) : null;
+       ("ant" in word_obj.verb) ? console.log("Adjective antonyms: " + word_obj.adjective.ant.join(', ')) : null;
+    }
+    
+    if ("sim" in word_obj){
+        console.log(colors.purple("Similar terms: " + word_obj.verb.syn.join(', ')));
+    }
+
     });
 });
 
-//Note to self: Prettify results.
