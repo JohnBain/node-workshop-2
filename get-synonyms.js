eb34786c9af5d4634c8ce999c13aac44f2c164be
jbain1 <jbainschool@gmail.com>
2016-04-20 00:11:32 +0000

    Made a functional thesaurus. Still serious bugs, working

--- get-synonyms.js
+++ get-synonyms.js
@@ -8,7 +8,15 @@ var syn1 = new SynonymAPI("c272336453182631b44cf03c0671b805");
 prompt.start();
 prompt.message = colors.blue("What word shall we look up today?");
 prompt.get(['word'], function (err, result) {
-    syn1.getSynonyms(result.word, function(n){console.log(n)});
+    syn1.getSynonyms(result.word, function(word_obj){
+        ("noun" in word_obj) ? console.log(colors.magenta("Noun synonyms: " + word_obj.noun.syn.join(', '))) : null;
+        ("ant" in word_obj.noun) ? console.log("Noun antonyms: " + word_obj.noun.ant.join(', ')) : null;
+        ("verb" in word_obj) ? console.log(colors.cyan("Verb synonyms: " + word_obj.verb.syn.join(', '))
+        + "\n Verb antonyms: " + word_obj.verb.ant.join(', ')) : null;
+        ("adjective" in word_obj) ? console.log(colors.blue("Adjective synonyms: " + word_obj.adjective.syn.join(', '))
+         + "\n Adjective antonyms: " + word_obj.verb.ant.join(', ')) : null;
+        ("sim" in word_obj) ? console.log(colors.purple("Similar terms: " + word_obj.verb.syn.join(', '))) : null; 
+    });
 });
 
 //Note to self: Prettify results.
